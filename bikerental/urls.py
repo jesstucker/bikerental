ab1a420b45997891b65f6cccd2faf12d0ddd31b0
Jess Tucker <jess.tucker@gmail.com>
2017-10-23 20:06:06 -0700

    MADE REACT STUFF

--- bikerental/urls.py
+++ bikerental/urls.py
@@ -13,12 +13,88 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from frontend import views as frontend_views
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+from inventory.models import Customer, Category, Group, ItemType, IndividualItem
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class CustomerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ('id', 'name', 'notes')
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class CustomerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ('id', 'name', 'notes')
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id','description','catg')
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    category = serializers.CharField(source='catg', read_only=True)
+    # category = serializers.RelatedField(source='catg')
+    class Meta:
+        model = Group        
+        fields = ('id', 'description', 'category',
+            )
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+#Customer,Category,Group,ItemType,IndividualItem
+class ItemTypeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ItemType
+        fields = ('id', 'name', 'group', 'cost_per_hour', 'cost_per_day', 'image')
+class ItemTypeViewSet(viewsets.ModelViewSet):
+    queryset = ItemType.objects.all()
+    serializer_class = ItemTypeSerializer
+
+
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'customer', CustomerViewSet)
+router.register(r'group', GroupViewSet)
+router.register(r'item-type', ItemTypeViewSet)
+
+
+
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', frontend_views.index),
     url(r'^catalog', frontend_views.catalog),
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
 ]
